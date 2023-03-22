ddd8ea34682fc253c5bf4c0968993f76f2d11760
brian <brianjsummerfield@gmail.com>
2023-03-22 01:39:26 -0500

    stone movement working

--- app/mancala/Board.tsx
+++ app/mancala/Board.tsx
@@ -9,21 +9,13 @@ enum GameState {
   InProgress = 2,
 }
 
-
-
-
 const Board: React.FC = () => {
   const [activePlayer, setActivePlayer] = useState<number>(GameState.PlayerOne)
   const [pits, setPits] = useState<number[]>(initialPits);
 
-  console.log(activePlayer)
-  const handlePitClick = (index: number) => {
-    const isPlayerOne = activePlayer === GameState.PlayerOne
-    if (isPlayerOne ? index > 5 : index < 6) return
-    
-    const newPits = [...pits];
-    let stones = newPits[index];
-    newPits[index] = 0;
+  const distributeStones = (pits: number[], index: number, isPlayerOne: boolean) => {
+    let stones = pits[index];
+    pits[index] = 0;
 
     let currentIndex = index;
     while (stones > 0) {
@@ -32,43 +24,49 @@ const Board: React.FC = () => {
 
       // Skip opponent's Mancala pit
       if (isPlayerOne ? currentIndex !== 13 : currentIndex !== 6) {
-        newPits[currentIndex]++;
+        pits[currentIndex]++;
         stones--;
       }
-      
     }
 
-    // Capture opponent's stones
-
-    if (
-      newPits[currentIndex] === 1 &&
-      (isPlayerOne ? currentIndex < 5 : currentIndex > 6)
-    ) {
-      const opponentIndex = isPlayerOne ? 12 - currentIndex : currentIndex - 12;
-      // newPits[currentIndex === 0 ? 13 : 6] += newPits[opponentIndex] + 1;
-      const currentPlayerMancala = isPlayerOne ? 6 : 13
-      newPits[currentPlayerMancala] += (newPits[currentIndex] + newPits[opponentIndex])
-      newPits[currentIndex] = 0;
-      newPits[opponentIndex] = 0;
-    }
-
-    // Switch turns if the last stone wasn't placed in the player's Mancala pit
-    // if (!((index < 6 && currentIndex === 6) || (index > 6 && currentIndex === 13))) {
-    //   const playerPits = newPits.slice(0, 6);
-    //   const opponentPits = newPits.slice(7, 13);
-
-    //   if (playerPits.every((pit) => pit === 0) || opponentPits.every((pit) => pit === 0)) {
-    //     newPits[6] += playerPits.reduce((sum, pit) => sum + pit, 0);
-    //     newPits[13] += opponentPits.reduce((sum, pit) => sum + pit, 0);
-    //     newPits.fill(0, 0, 6).fill(0, 7, 13);
-    //   } else {
-    //     // newPits.reverse(); // Switch turns
-    //   }
-    // }
-    setActivePlayer(1 - activePlayer)
-    setPits(newPits);
-
+    return currentIndex;
   };
+
+  const isPlayersPit = (index: number, isPlayerOne: boolean) => {
+    return isPlayerOne ? index < 5 : index > 6
+  }
+
+  const captureOpponentStones = (
+    pits: number[],
+    lastIndex: number,
+    currentPlayerMancala: number,
+    isPlayerOne: boolean
+  ) => {
+    if (pits[lastIndex] === 1 && isPlayersPit(lastIndex, isPlayerOne)) {
+      const opponentIndex = isPlayerOne ? 12 - lastIndex : lastIndex - 12;
+      pits[currentPlayerMancala] += pits[lastIndex] + pits[opponentIndex];
+      pits[lastIndex] = 0;
+      pits[opponentIndex] = 0;
+    }
+  };
+
+  const handlePitClick = (index: number) => {
+    const isPlayerOne = activePlayer === GameState.PlayerOne;
+    if (!isPlayersPit(index, isPlayerOne)) return;
+
+    const currentPlayerMancala = isPlayerOne ? 6 : 13;
+
+    const newPits = [...pits];
+
+    const lastIndex = distributeStones(newPits, index, isPlayerOne);
+    captureOpponentStones(newPits, lastIndex, currentPlayerMancala, isPlayerOne);
+
+    if (lastIndex !== currentPlayerMancala) {
+      setActivePlayer(1 - activePlayer);
+    }
+    setPits(newPits);
+  };
+
 
   const topPits = pits.slice(7, 13).reverse();
   const bottomPits = pits.slice(0, 6);
